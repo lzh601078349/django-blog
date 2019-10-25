0cf89a23a1abd1edf648a1f39a8ccb3e353d2127
lzh <601078349@qq.com>
2019-10-25 20:53:13 +0800

    all

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=)e(qrb^=t_=g&ra&a_*b9nb*!wt2ip+d6-6(=kjmr6mk@dirl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.lzhamoiowa.com']
 
 # Application definition
@@ -120,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
